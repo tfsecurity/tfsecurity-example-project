e9c318879fdd492900b7769dd1813446102b8704
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2023-08-04 01:59:09 +0600

    Create main.tf

---
+++ modules/acme_bucket/main.tf
@@ -0,0 +1,11 @@
+resource "aws_s3_bucket" "acme_bucket" {
+  bucket = var.bucket_name
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
